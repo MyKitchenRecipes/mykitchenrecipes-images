3e3fc31af765a4c05f7f47e6b14c271ca2b33c94
Nora Söderlund <nora.soderlund@polestar.com>
2024-01-13 22:52:08 +0100

    Improve quality

--- app/pages/src/components/App.js
+++ app/pages/src/components/App.js
@@ -9,17 +9,39 @@ function App() {
 
     const url = searchParams.get("url");
 
-    removeBackground(url, {
-      publicPath: `${window.location.protocol + '//' + window.location.host + window.location.pathname}/static/js/`,
-      model: "small",
-      output: {
-        quality: 0.5
-      }
-    }).then((imageBlob) => {
-      const url = URL.createObjectURL(imageBlob);
+    const image = new Image();
 
-      setImageUrl(url);
-    });
+    image.onload = () => {
+      const paddingSize = Math.round(Math.max(image.width, image.height) / 10);
+
+      const width = paddingSize + image.width + paddingSize;
+      const height = paddingSize + image.height + paddingSize;
+
+      const canvas = new OffscreenCanvas(width, height);
+      const context = canvas.getContext("2d");
+
+      context.fillStyle = "#000";
+      context.fillRect(0, 0, width, height);
+
+      context.drawImage(image, 0, 0, width, height, paddingSize, paddingSize, width, height);
+
+      canvas.convertToBlob().then((blob) => {
+        removeBackground(blob, {
+          publicPath: `${window.location.protocol + '//' + window.location.host + window.location.pathname}/static/js/`,
+          model: "medium",
+          output: {
+            quality: 0
+          }
+        }).then((imageBlob) => {
+          const url = URL.createObjectURL(imageBlob);
+    
+          setImageUrl(url);
+        });
+      });
+    };
+
+    image.crossOrigin = "anonymous";
+    image.src = url;
   }, []);
 
   return (imageUrl) && (
